b952956074b56c031ba366eabcc5c51cf65f9c2c
Ashutosh Vyas <ashutoshvyas.vyas@gmail.com>
2026-03-26 17:36:23 +0530

    feat: add responsive navbar with mobile navigation and theme toggler

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,11 +1,39 @@
+import { NavLink } from '@/constant/constant'
+import Link from 'next/link'
 import React from 'react'
+import { CgClose } from 'react-icons/cg'
 
-const MobileNav = () => {
-  return (
-    <div>
-      MobileNav
-    </div>
-  )
+type props = {
+   showNav:boolean;
+   closeNav: () => void;
+
+}
+
+const MobileNav = ({showNav,closeNav}:props) => {
+
+    const navOpen = showNav ? "translate-x-0":"translate-x-[100%]"
+
+    return (
+        <div>
+            {/* Overlay */}
+            <div className={`fixed ${navOpen} insert-0 transform transition-all right-0 duration-500 z-100002 bg-black opacity-70 w-full h-screen `}></div>
+            {/* NavLinks */}
+            <div className={`text-white ${navOpen} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-cyan-800 space-y-6 z-1000050 right-0`}>
+                
+                {NavLink.map((link)=>{
+                    return (
+                    <Link key={link.id} href={link.Lable}>
+                        <p className='text-white w-fit text-xl ml-12 border-b-[1.5px] pb-1 border-white 
+                        sm:text-[30px'>{link.Lable}</p>
+                    </Link>
+                    )
+                })}
+
+                {/* Close Icon*/}
+                    <CgClose onClick={closeNav} className='absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6'/>
+            </div>
+        </div>
+    )
 }
 
 export default MobileNav
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,19 +1,41 @@
+"use client"
+
 import { NavLink } from '@/constant/constant'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HiBars3BottomRight } from 'react-icons/hi2'
 import { LuNetwork } from 'react-icons/lu'
+import ThemeToggler from './../../Helper/ThemeToggler';
 
-const Nav = () => {
+type props = {
+    openNav: () => void;
+
+}
+
+const Nav = ({openNav}:props) => {
+    const [navBg, setNavBg] = useState(false);
+
+    useEffect(() => {
+        const handler = () => {
+            if (window.scrollY >= 90) setNavBg(true);
+            if (window.scrollY < 90) setNavBg(false);
+        };
+
+        window.addEventListener("scroll", handler);
+
+        return () => window.removeEventListener("scroll", handler)
+
+    }, []);
     return (
-        <div className="transition-all duration-200 h-12vh z-10000 fixed w-full ">
-            <div className="flex items-center- h-full justify-between w-92% mx-15 my-6">
+        <div className={`transition-all ${navBg ? "bg-white dark:bg-gray-900 shadow-md" : "fixed"} duration-200 h-12vh z-10000 fixed w-full `}>
+            <div className="flex items-center- h-full justify-between w-92% mx-4 sm:mx-6 md:mx-8 lg:mx-4 xl:mx-6 my-6">
                 <div className="flex items-center sm:space-x-20">
                     {/* Nav Logo */}
                     <div className="flex items-center space-x-2">
-                        <div className="w-10 h-10 bg-cyan-800 rounded-full flex items-center justify-center flex-col">
-                            <LuNetwork className="w-5 h-5 text-white" />
+                        <div className="w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center flex-col">
+                            <LuNetwork className="w-5 h-5 text-white dark:text-black" />
                         </div>
-                        <h1 className='text-1xl hidden sm:block md:text-2xl text-cyan-800 font-bold'>
+                        <h1 className='text-1xl hidden sm:block md:text-2xl text-cyan-800 dark:text-white font-bold'>
                             DevHire
                         </h1>
                     </div>
@@ -25,7 +47,7 @@ const Nav = () => {
                                 <Link
                                     key={link.id}
                                     href={link.url}
-                                    className='text-base hover:text-cyan-700 font-medium transition-all duration-200'>
+                                    className='text-base hover:text-cyan-700  dark:text-cyan-200 font-medium transition-all duration-200'>
                                     <p>{link.Lable}</p>
                                 </Link>
                             )
@@ -44,15 +66,17 @@ const Nav = () => {
                         rounded-lg 
                         cursor-pointer
                         bg-gray-100
+                         dark:bg-gray-700
+                          dark:hover:bg-gray-900
                         hover:bg-gray-300
                         transition-all
                         duration-300
                         '
-                         >
-                            Login/register
+                    >
+                        Login/register
                     </button>
                     {/* Job post buttons */}
-                        <button 
+                    <button
                         className='
                             px-8 
                             py-2.5
@@ -67,12 +91,12 @@ const Nav = () => {
                             duration-300
                             text-white
                             '>
-                            Job Post
-                        </button>
-                        {/* Theme Toggler */}
-
-                        {/* Burger Menu*/}
-
+                        Job Post
+                    </button>
+                    {/* Theme Toggler */}
+                        <ThemeToggler />
+                    {/* Burger Menu*/}
+                    <HiBars3BottomRight className='w-8 h-8 cursor-pointer text-black lg:hidden' onClick={openNav} />
                 </div>
 
             </div>
